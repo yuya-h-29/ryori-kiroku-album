4b2068e80dd8ba21ceb6ba19ecf27347eba4292a
yuya-h-29 <tzou11291996@gmail.com>
2020-09-26 21:46:56 +0900

    import alamofire and swiftyJSON

---
+++ ryori-kiroku-album/Controllers/RecipeListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeListViewController.swift
+//  ryori-kiroku-album
+//
+//  Created by Yuya Harada on 2020/09/26.
+//  Copyright © 2020 Yuya Harada. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class RecipeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ryori-kiroku-album/Controllers/RecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeViewController.swift
+//  ryori-kiroku-album
+//
+//  Created by Yuya Harada on 2020/09/26.
+//  Copyright © 2020 Yuya Harada. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
