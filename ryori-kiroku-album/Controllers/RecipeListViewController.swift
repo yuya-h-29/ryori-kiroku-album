d646c3a62f5c118f1de4622b1648972ef39481a5
yuya-h-29 <tzou11291996@gmail.com>
2020-09-26 22:41:00 +0900

    get JSON data using Alamofire & SwiftyJSON

--- ryori-kiroku-album/Controllers/RecipeListViewController.swift
+++ ryori-kiroku-album/Controllers/RecipeListViewController.swift
@@ -15,6 +15,33 @@ class RecipeListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        AF.request("https://cooking-records.herokuapp.com/cooking_records").responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                
+                
+                let list: Array<JSON> = json["cooking_records"].arrayValue
+                
+                let imageUrlArr = list.map{$0["image_url"].stringValue}
+                
+                let commentArr = list.map{$0["comment"].stringValue}
+                
+                let recipeTypeArr = list.map{$0["recipe_type"].stringValue}
+                
+                let dateArr = list.map{$0["recorded_at"].stringValue}
+                
+                debugPrint(imageUrlArr)
+                debugPrint(commentArr)
+                debugPrint(recipeTypeArr)
+                debugPrint(dateArr)
+                
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
         
 
 
