b5c3144b5f0c8ca5524ee309fb06845d1ba77997
yuya-h-29 <tzou11291996@gmail.com>
2020-09-28 11:58:54 +0900

    create recipe model

---
+++ ryori-kiroku-album/Model/RecipeData.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeData.swift
+//  ryori-kiroku-album
+//
+//  Created by Yuya Harada on 2020/09/28.
+//  Copyright © 2020 Yuya Harada. All rights reserved.
+//
+
+import Foundation
+
+
+struct RecipeData {
+    
+    let recipeType: String
+    let recordedAt: String
+    let imageUrl: String
+    let comment: String
+    
+}
